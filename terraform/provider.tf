6da621af4a9f28552a75d803176d79b8bf753e02
Ahmad Ragab <averroes2006@gmail.com>
2024-10-11 18:35:40 -0400

    initial commit

---
+++ terraform/provider.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  cloud { 
+    organization = "north-demo" 
+
+    workspaces { 
+      name = "missouri" 
+    } 
+  } 
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.3"
+    }
+  }
+}
